446d0fb4c906461182529852d891c673c64dfb67
Ta Duy Duc <duc@ducdigital.com>
2016-11-09 11:28:12 +0100

    Update check

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const _ = require('lodash');
 const notifier = require('node-notifier');
 
 const HOST = 'https://www.ticketswap.com';
-const EVENT_URL = '/event/lost-in-a-moment-saint-malo/7f896c0e-7fdc-41e1-a671-9fcecd39e64d';
+const EVENT_URL = '/event/flume-world-tour/1afe7e5c-2609-43e6-ae18-f466109fdb13';
 const CHECK_INTERVAL_MS = 200000;
 
 let fetchResult = function (link) {
@@ -17,10 +17,31 @@ let fetchResult = function (link) {
         method: 'GET'
     });
 
-    let $ = cheerio.load(result.body);
-
-    return $('.counter-available .counter-value').text();
+    return cheerio.load(result.body);
   });
+};
+
+let botAction = {
+  robotCheck: function (url) {
+    console.log(`${url} : ---> Need to visit and check if you are block as robot`);
+    return notifier.notify({
+      'title': 'Need check as robot',
+      'message': url,
+      'open': url,
+    });
+  },
+  availableTicket: function (url, data) {
+    console.log(`${url} : ---> ${data} available`);
+    return notifier.notify({
+      'title': 'New ticket!!!',
+      'message': url,
+      'open': url,
+    });
+  },
+  unavailableTicket: function (url, data) {
+    console.log(`${url} : ---> Not available`);
+    return false;
+  }
 };
 
 let app = function () {
@@ -29,39 +50,42 @@ let app = function () {
         uri: HOST + EVENT_URL,
         method: 'GET'
     });
+
     let $ = cheerio.load(result.body);
     let hasData = false;
     let linksFn = {};
 
-    $('.events-list--item a').each((index, link) => {
-      let fetchUrl = HOST + _.get(link, 'attribs.href');
-      linksFn[fetchUrl] = fetchResult(fetchUrl);
-    });
+    if ($('.events-list--item a').length > 0) {
+      $('.events-list--item a').each((index, link) => {
+        let fetchUrl = HOST + _.get(link, 'attribs.href');
+        linksFn[fetchUrl] = fetchResult(fetchUrl);
+      });
+    } else {
+      linksFn[HOST + EVENT_URL] = Promise.resolve($);
+    }
 
     let linksResults = yield linksFn;
 
-    _.each(linksResults, function (res, key) {
-      if (res === '') {
-        console.log(`${key} : ---> Need to visit and check if you are block as robot`);
-        return notifier.notify({
-          'title': 'Need check as robot',
-          'message': key,
-          'open': key,
-        });
+    _.each(linksResults, function ($query, url) {
+      let counterValue = $query('.counter-available .counter-value').text();
+      let isRobotCheck = $query('.counter-available .counter-value');
+
+      if ($query('#recaptcha').length > 0) {
+        return botAction.robotCheck(url);
       }
 
-      if (parseInt(res, 10) > 0) {
-        console.log(`${key} : ---> ${res} available`);
-        return notifier.notify({
-          'title': 'New ticket!!!',
-          'message': key,
-          'open': key,
-        });
+      if (parseInt(counterValue, 10) > 0) {
+        return botAction.availableTicket(url, counterValue);
       }
 
-      console.log(`${key} : ---> Not available`);
+      return botAction.unavailableTicket(url);
     });
+
+    return true;
+  }).catch(ex => {
+    console.log(ex);
   });
 };
 
+app();
 setInterval(app, CHECK_INTERVAL_MS);
