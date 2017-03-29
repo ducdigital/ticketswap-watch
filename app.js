9ec835483d502e07059544e69a47d1c579d17d32
Ta Duy Duc <duc@ducdigital.com>
2017-03-29 16:55:21 +0200

    Update

--- app.js
+++ app.js
@@ -5,14 +5,17 @@ const request = require('co-request');
 const cheerio = require('cheerio');
 const _ = require('lodash');
 const notifier = require('node-notifier');
+const exec = require('child_process').exec;
 
 const HOST = 'https://www.ticketswap.com';
-const EVENT_URL = '/event/flume-world-tour/1afe7e5c-2609-43e6-ae18-f466109fdb13';
-const CHECK_INTERVAL_MS = 200000;
+const EVENT_URL = '/event/h-a-i-k-u-avec-dixon/3ba089c9-1d9a-4851-8635-1332198449c1';
+const CHECK_INTERVAL_MS = 60000;
 
 let cookieJar = request.jar();
 
 let buildRequest = function (uri, method) {
+  console.log(`Fetching: ${uri}`);
+
   return request({
     uri: uri,
     method: method,
@@ -42,6 +45,7 @@ let botAction = {
   },
   availableTicket: function (url, data) {
     console.log(`${url} : ---> ${data} available`);
+    exec(`open ${url}`);
     return notifier.notify({
       'title': 'New ticket!!!',
       'message': url,
@@ -75,7 +79,8 @@ let app = function () {
 
     _.each(linksResults, function ($query, url) {
       let counterValue = $query('.counter-available .counter-value').text();
-      let isRobotCheck = $query('.counter-available .counter-value');
+
+      console.log(parseInt(counterValue, 10));
 
       if ($query('#recaptcha').length > 0) {
         return botAction.robotCheck(url);
